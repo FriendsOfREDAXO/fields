cd6feeaa28b36b9d9b9cc59e010a53e6881990d5
Thomas Skerbis <thomas.skerbis@mac.com>
2026-02-15 15:13:01 +0100

    numbe wie original

--- lib/yform/value/fields_inline_number.php
+++ lib/yform/value/fields_inline_number.php
@@ -12,36 +12,45 @@ class rex_yform_value_fields_inline_number extends rex_yform_value_abstract
 {
     public function enterObject()
     {
-        // 1. Formular-Ansicht
-        if ($this->needsOutput() && $this->isViewable()) {
-            if (!$this->isEditable()) {
-                $this->params['form_output'][$this->getId()] = $this->parse('value.showvalue.tpl.php');
-            } else {
-                $attributes = [
-                    'class' => 'form-control',
-                    'name'  => $this->getFieldName(),
-                    'id'    => $this->getFieldId(),
-                    'type'  => 'number',
-                    'step'  => $this->getElement('step') ?: 'any',
-                    'min'   => $this->getElement('min'),
-                    'max'   => $this->getElement('max'),
-                ];
-
-                $attributes = $this->getAttributeElements($attributes, ['placeholder', 'autocomplete', 'pattern', 'required', 'disabled', 'readonly']);
-
-                $this->params['form_output'][$this->getId()] = $this->parse('value.text.tpl.php', [
-                    'type' => 'number',
-                    'prepend' => $this->getElement('prefix'),
-                    'append' => $this->getElement('suffix'),
-                ]);
-            }
+        if ('' == $this->getValue() && !$this->params['send']) {
+            $this->setValue($this->getElement('default'));
         }
 
-        // 2. Datenbank-Speicherung
+        if ('' === $this->getValue()) {
+            $this->setValue(null);
+        } else {
+            $this->setValue($this->getValue());
+        }
+
         $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
         if ($this->saveInDb()) {
             $this->params['value_pool']['sql'][$this->getName()] = $this->getValue();
         }
+
+        if (!$this->needsOutput() || !$this->isViewable()) {
+            return;
+        }
+
+        if (!$this->isEditable()) {
+            $this->params['form_output'][$this->getId()] = $this->parse(
+                ['value.number-view.tpl.php', 'value.integer-view.tpl.php', 'value.view.tpl.php'],
+                ['prepend' => $this->getElement('unit')],
+            );
+        } else {
+            $type = 'text';
+            if ('input:number' == $this->getElement('widget')) {
+                $type = 'number';
+            }
+            $this->params['form_output'][$this->getId()] = $this->parse(
+                ['value.number.tpl.php', 'value.integer.tpl.php', 'value.text.tpl.php'],
+                ['prepend' => $this->getElement('unit'), 'type' => $type],
+            );
+        }
+    }
+
+    public function getDescription(): string
+    {
+        return 'fields_inline_number|name|label|precision|scale|defaultwert|[no_db]|[unit]|[notice]|[attributes]';
     }
 
     public function getDefinitions(): array
@@ -52,15 +61,32 @@ class rex_yform_value_fields_inline_number extends rex_yform_value_abstract
             'values' => [
                 'name' => ['type' => 'name',    'label' => rex_i18n::msg('yform_values_defaults_name')],
                 'label' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_label')],
-                'min' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_number_min')],
-                'max' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_number_max')],
-                'step' => ['type' => 'text',   'label' => 'Step (z.B. 0.01)', 'default' => 'any'],
-                'prefix' => ['type' => 'text', 'label' => rex_i18n::msg('fields_inline_number_prefix'), 'notice' => rex_i18n::msg('fields_inline_number_prefix_notice')],
-                'suffix' => ['type' => 'text', 'label' => rex_i18n::msg('fields_inline_number_suffix'), 'notice' => rex_i18n::msg('fields_inline_number_suffix_notice')],
+                'precision' => ['type' => 'integer', 'label' => rex_i18n::msg('yform_values_number_precision'), 'default' => '10'],
+                'scale' => ['type' => 'integer', 'label' => rex_i18n::msg('yform_values_number_scale'), 'default' => '2'],
+                'default' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_number_default')],
+                'no_db' => ['type' => 'no_db',   'label' => rex_i18n::msg('yform_values_defaults_table'),  'default' => 0],
+                'widget' => ['type' => 'choice', 'label' => rex_i18n::msg('yform_values_defaults_widgets'), 'choices' => ['input:text' => 'input:text', 'input:number' => 'input:number'], 'default' => 'input:text'],
+                'unit' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_unit')],
                 'notice' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_notice')],
+                'attributes' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_attributes'), 'notice' => rex_i18n::msg('yform_values_defaults_attributes_notice')],
+            ],
+            'validates' => [
+                ['type' => ['name' => 'precision', 'type' => 'integer', 'message' => rex_i18n::msg('yform_values_number_error_precision', '1', '65'), 'not_required' => false]],
+                ['type' => ['name' => 'scale', 'type' => 'integer', 'message' => rex_i18n::msg('yform_values_number_error_scale', '0', '30'), 'not_required' => false]],
+                ['compare' => ['name' => 'scale', 'name2' => 'precision', 'message' => rex_i18n::msg('yform_values_number_error_compare'), 'compare_type' => '>']],
+                ['intfromto' => ['name' => 'precision', 'from' => '1', 'to' => '65', 'message' => rex_i18n::msg('yform_values_number_error_precision', '1', '65')]],
+                ['intfromto' => ['name' => 'scale', 'from' => '0', 'to' => '30', 'message' => rex_i18n::msg('yform_values_number_error_scale', '0', '30')]],
             ],
             'description' => rex_i18n::msg('fields_inline_number_description'),
-            'db_type' => ['int', 'float', 'double', 'decimal', 'varchar'],
+            'db_type' => ['DECIMAL({precision},{scale})'],
+            'hooks' => [
+                'preCreate' => static function (rex_yform_manager_field $field, $db_type) {
+                    $db_type = str_replace('{precision}', (string) ($field->getElement('precision') ?? 6), $db_type);
+                    $db_type = str_replace('{scale}', (string) ($field->getElement('scale') ?? 2), $db_type);
+                    return $db_type;
+                },
+            ],
+            'db_null' => true,
         ];
     }
 
@@ -82,23 +108,24 @@ class rex_yform_value_fields_inline_number extends rex_yform_value_abstract
         }
 
         $fieldName = $field['name'];
-        $prefix = isset($field['prefix']) ? $field['prefix'] : '';
-        $suffix = isset($field['suffix']) ? $field['suffix'] : '';
-        
+        // Unit from Definition (original 'unit' param)
+        $unit = isset($field['unit']) ? $field['unit'] : '';
+
+        // Formatting based on scale/precision if available
+        $formattedValue = rex_escape($value);
+        if ($value !== '' && $value !== null && isset($field['scale']) && is_numeric($field['scale'])) {
+            $formattedValue = number_format((float)$value, (int)$field['scale'], ',', '.');
+        }
+
         // CSRF Token
         $token = rex_csrf_token::factory('fields_inline_edit')->getValue();
         
-        // Display Logic
-        $displayValue = rex_escape($value);
-        if ($displayValue === '') {
+        $displayValue = $formattedValue;
+        if ((string)$value === '') {
              $displayValue = '&nbsp;<i class="rex-icon fa-pencil" style="opacity:0.3"></i>&nbsp;';
         } else {
-             // Wrap value with prefix/suffix for display
-             if ($prefix) {
-                 $displayValue = '<span class="fields-inline-prefix">'.rex_escape($prefix).'</span> ' . $displayValue;
-             }
-             if ($suffix) {
-                 $displayValue = $displayValue . ' <span class="fields-inline-suffix">'.rex_escape($suffix).'</span>';
+             if ($unit) {
+                 $displayValue = $displayValue . ' ' . '<span class="text-muted">'.rex_escape($unit).'</span>';
              }
         }
 
@@ -110,8 +137,6 @@ class rex_yform_value_fields_inline_number extends rex_yform_value_abstract
                   data-type="number"
                   data-token="%s"
                   data-raw-value="%s"
-                  data-prefix="%s"
-                  data-suffix="%s"
                   title="%s">
                 <div class="fields-inline-view">%s</div>
                 <div class="fields-inline-input" style="display:none;"></div>
@@ -121,8 +146,6 @@ class rex_yform_value_fields_inline_number extends rex_yform_value_abstract
             (int) $id,
             $token,
             rex_escape($value),
-            rex_escape($prefix),
-            rex_escape($suffix),
             rex_i18n::msg('edit'),
             $displayValue
         );
